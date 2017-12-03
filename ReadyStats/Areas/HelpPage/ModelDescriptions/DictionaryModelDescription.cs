a6d88f1c34f28e112bc17e9be5be38a899ab4e27
Robert Wells <rwells2@jdbyrider.com>
2017-12-03 10:44:10 -0500

    Add project files.

---
+++ ReadyStats/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ReadyStats.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ReadyStats/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ReadyStats.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
